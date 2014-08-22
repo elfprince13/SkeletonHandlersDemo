b312a33f0be379f9856188047d457b277998d419
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2014-08-22 14:46:58 -0400

    Build system appears to work with Xcode and Make

---
+++ lib/ClangSkelImpl-Tests/PluginMain.cpp
@@ -0,0 +1,14 @@
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/Frontend/CompilerInstance.h"
+
+#include "clang/Parse/Parser.h"
+
+using namespace clang;
+
+
+
+Parser::StmtResult doParseSkeletonWithName_Loop2dAccumulate(SourceLocation kindLoc){
+	return StmtResult();
+}
